27db61466a94e2e2c7bed17595dcf06f7032fa24
mfhan <mf212mf@gmail.com>
2019-08-29 09:45:56 -0400

    Updates

---
+++ precache-manifest.cde4a523bfd73e78b43f17d1163223f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b0662d3ae48cbb5bd8904c5f77120f",
+    "url": "/newswindow/index.html"
+  },
+  {
+    "revision": "10136c98dc02d76c9a0a",
+    "url": "/newswindow/static/css/main.cca48d7a.chunk.css"
+  },
+  {
+    "revision": "326b7b6488da74d75bc8",
+    "url": "/newswindow/static/js/2.fa1e7d00.chunk.js"
+  },
+  {
+    "revision": "10136c98dc02d76c9a0a",
+    "url": "/newswindow/static/js/main.6be1cdbc.chunk.js"
+  },
+  {
+    "revision": "d385c1eb27456006e1aa",
+    "url": "/newswindow/static/js/runtime~main.e092415e.js"
+  }
+]);
